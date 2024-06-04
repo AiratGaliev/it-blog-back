e8f72ba19a7f44ab5baf60a09cd6d3fc1961c725
Airat Galiev <airat.galiev@gmail.com>
2024-06-04 21:38:23 +0300

    created new project

---
+++ src/main/java/com/github/airatgaliev/itblogback/ItBlogBackApplication.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.itblogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBlogBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItBlogBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/airatgaliev/itblogback/ItBlogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.itblogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBlogBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
