3cf6e7ca829a824849f2498094296ffe35afe5ac
Airat Galiev <airat.galiev@gmail.com>
2024-07-20 07:57:09 +0300

    fix getting token from cookie

--- src/main/java/com/github/airatgaliev/itblogback/security/JwtAuthenticationFilter.java
+++ src/main/java/com/github/airatgaliev/itblogback/security/JwtAuthenticationFilter.java
@@ -33,7 +33,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     String jwt = null;
     if (cookies != null) {
       for (Cookie cookie : cookies) {
-        if ("token".equals(cookie.getName())) {
+        if ("auth-token".equals(cookie.getName())) {
           jwt = cookie.getValue();
           break;
         }
